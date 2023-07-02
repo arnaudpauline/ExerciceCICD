a34a04d7b6d0f066ecf427b245f4afa57a5524ac
arnaudpauline <70948234+arnaudpauline@users.noreply.github.com>
2023-07-02 09:43:57 +0200

    Update

--- ProjetExerciceCICD/Startup.cs
+++ ProjetExerciceCICD/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -45,6 +46,10 @@ namespace ProjetExerciceCICD
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapGet("/hello", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
